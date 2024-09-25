275b9ce100f3c0fa49f3022401c84fc45ca15bc5
ayseugurlu <ayseugurlu9135@gmail.com>
2024-09-25 13:07:36 +0200

    "Deleted entire ts-03-first-app project directory"

---
+++ class-notes/Ts-03-rct-todoapp/src/components/AddToDoComp.tsx
@@ -0,0 +1,85 @@
+import { Box, Button, TextField } from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+import { useState } from "react";
+
+//! 1. Kullanım yolu props type
+// const AddTodoComp = ({
+//   addTodo,
+// }: {
+//   addTodo: (text: string) => Promise<void>;
+// }) => {
+//   // const [text,setText] = useState<string>("")
+//   const [text, setText] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
+//   const handleClick = () => {
+//     addTodo(text);
+//     setText("");
+//   };
+//   return (
+//     <Box>
+//       <TextField
+//         id="outlined-disabled"
+//         label="New Todo"
+//         variant="outlined"
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       />
+//       <Button
+//         variant="contained"
+//         color="success"
+//         disabled={!text.trim()}
+//         onClick={handleClick}
+//         endIcon={<SaveIcon />}
+//       >
+//         Save Todo
+//       </Button>
+//     </Box>
+//   );
+// };
+
+// export default AddTodoComp;
+
+
+//* 2. Kullanım yolu props type
+interface IAddTodoComp {
+    // addTodo: (text: string) => Promise<void>;
+    addTodo: AddFn;
+}
+
+const AddToDoComp = ({addTodo}: IAddTodoComp ) => {
+    // const [text,setText] = useState<string>("")
+    const [text, setText] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
+    const handleClick = () => {
+      addTodo(text);
+      setText("");
+    };
+    return (
+      <Box sx={{
+        display: { xs: "block", sm: "flex" },
+        justifyContent: { xs: "flex-start", sm: "center" },
+        m: { xs: 1, sm: "auto" },
+        height: { xs: "120px", sm: "80px" },
+      }}>
+        <TextField
+          id="outlined-disabled"
+          label="New Todo"
+          variant="outlined"
+          color="success"
+          sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+          disabled={!text.trim()}
+          onClick={handleClick}
+          endIcon={<SaveIcon />}
+        >
+          Save Todo
+        </Button>
+      </Box>
+    );
+  };
+  
+  export default AddToDoComp;
---
+++ class-notes/Ts-03-rct-todoapp/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+
+const Header = () => {
+  return (
+    <Typography color="error" component="h1" variant='h2'>
+        Todo App With TypeScript
+    </Typography>
+  )
+}
+
+export default Header
